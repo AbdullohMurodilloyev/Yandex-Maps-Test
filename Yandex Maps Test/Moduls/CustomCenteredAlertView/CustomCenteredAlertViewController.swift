0b36432d105a61c53e995256ea11a7c07515fe98
AbdullohMurodilloyev <abdullohmurodilloyev98@gmail.com>
2025-02-20 04:01:15 +0500

    finished UI

---
+++ Yandex Maps Test/Moduls/CustomCenteredAlertView/CustomCenteredAlertViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomCenteredAlertViewController.swift
+//  Yandex Maps Test
+//
+//  Created by Abdulloh Murodilloyev on 20/02/25.
+//
+
+import UIKit
+
+class CustomCenteredAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Yandex Maps Test/Moduls/SearchResults/SearchResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultsViewController.swift
+//  Yandex Maps Test
+//
+//  Created by Abdulloh Murodilloyev on 19/02/25.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
